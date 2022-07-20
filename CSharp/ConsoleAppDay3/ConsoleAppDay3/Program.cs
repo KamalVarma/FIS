682f12e04b86c4beb840e594019a28c855a19fb5
BanurekhaV <rekhasairam@hotmail.com>
2022-07-20 11:08:46 +0530

    Access Specifiers

---
+++ CSharp/ConsoleAppDay3/ConsoleAppDay3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay3
+{
+    public class Employee
+    {
+        int Empid;
+       public string Empname;
+        DateTime DOJ;
+        float Salary;
+
+       public void Accept()
+        {
+            Console.WriteLine("Enter Employee id, name, doj,salary");
+            Empid = Convert.ToInt32(Console.ReadLine());
+            Empname = Console.ReadLine();
+            DOJ = Convert.ToDateTime(Console.ReadLine());
+            Salary = Convert.ToSingle(Console.ReadLine());
+        }
+
+       public void Display()
+        {
+            Console.WriteLine($"Empid :{Empid}, Name :{Empname}, Doj: {DOJ} and Salary: {Salary}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Accept();
+            employee.Display();
+            employee.Empname = "Shinde";
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/ConsoleAppDay3/Project2/Project2Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleAppDay3;
+
+namespace Project2
+{
+    class Project2Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Empname = "Mohit";
+            Console.WriteLine(emp.Empname);
+            emp.Accept();
+            emp.Display();
+            Console.Read();
+        }
+    }
+}
